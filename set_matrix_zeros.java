2ad6f6a90a7074af3a7443ab77a96c9c4fad3862
Murugalakshmi <muruga6805@gmail.com>
2025-12-18 19:14:39 +0530

    Create set_matrix_zeros.java

---
+++ set_matrix_zeros.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        boolean firstrowhaszero=false;
+        boolean firstcolhaszero=false;
+        for(int j=0;j<cols;j++){
+            if(matrix[0][j]==0){
+                firstrowhaszero=true;
+                break;
+            }
+        }
+        for(int i=0;i<rows;i++){
+            if(matrix[i][0]==0){
+                firstcolhaszero=true;
+                break;
+
+            }
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<rows;i++){
+            if(matrix[i][0]==0){
+                for(int j=1;j<cols;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        for(int j=1;j<cols;j++){
+            if(matrix[0][j]==0){
+                for(int i=1;i<rows;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstrowhaszero){
+            for(int j=0;j<cols;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstcolhaszero){
+            for(int i=0;i<rows;i++){
+                matrix[i][0]=0;
+            }
+        }
+
+            }
+        }
